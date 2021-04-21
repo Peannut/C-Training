1a64f859a8ad3ffa9af0cf252e71d08e03420547
P3RPL3Xs <80137110+P3RPL3Xs@users.noreply.github.com>
2021-04-21 17:05:36 +0000

    Create Perplex1.c

---
+++ Perplex1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void toUpperCase(char arr[]) {  
+    int i,max;
+    for(i = 0; arr[i] != '\0'; i++)
+    {
+    max = ((i % 2) == 0 && arr[i]>='a' && arr[i]<='z') ? (arr[i] = arr[i]-32):(i % 2 != 0 && arr[i]>='A' && arr[i]<='Z')?(arr[i] = arr[i]+32):0;
+    }
+    printf("%s\n", arr);    
+}
+int main(){
+  char t[] = "PerPlexS";
+  toUpperCase(t);
+  return 0;
+}
+
